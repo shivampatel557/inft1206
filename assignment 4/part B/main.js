7418d908fdc5114452f32f524501f75879e6b97e
shivampatel557 <shivam.patel17@dcmail.ca>
2025-04-03 20:57:54 -0400

    Update main.js
    creating the js code for the immage to get poped up.

--- assignment 4/part B/main.js
+++ assignment 4/part B/main.js
@@ -15,3 +15,30 @@ const alts = {
   'pic5.jpg' : 'Large moth on a leaf'
 }
 
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
